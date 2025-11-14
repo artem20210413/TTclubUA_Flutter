b0a062b6150d2c9c910dbc387968afd29a10dd10
Artem Tishchenko <82469944+artem20210413@users.noreply.github.com>
2025-11-14 15:14:20 +0200

    fixed save area

--- lib/Storage/Cache/DeviceInsetsCache.dart
+++ lib/Storage/Cache/DeviceInsetsCache.dart
@@ -1,13 +1,23 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 
 class DeviceInsetsCache {
-  static double? _notchHeight;
+  static double? _viewPaddingTop;
+  static double? _viewPaddingBottom;
 
-  static double get notchHeight => _notchHeight ?? 0;
+  static double get viewPaddingTop => _viewPaddingTop ?? 0;
+
+  static double get viewPaddingBottom => _viewPaddingBottom ?? 0;
+
+  static bool isNavigationButtonsAndroid =
+      Platform.isIOS == false && DeviceInsetsCache.viewPaddingBottom > 40;
 
   static void init(BuildContext context) {
-    if (_notchHeight == null) {
-      _notchHeight = MediaQuery.of(context).viewPadding.top;
-    }
+    if (_viewPaddingTop == null)
+      _viewPaddingTop = MediaQuery.of(context).viewPadding.top;
+
+    if (_viewPaddingBottom == null)
+      _viewPaddingBottom = MediaQuery.of(context).viewPadding.bottom;
   }
 }
--- lib/pages/Nav/Home.dart
+++ lib/pages/Nav/Home.dart
@@ -37,7 +37,7 @@ class _HomeState extends State<Home> {
         padding: const EdgeInsets.fromLTRB(16, 0, 16, 113),
         child: Column(
           children: [
-            SizedBox(height: DeviceInsetsCache.notchHeight + 6),
+            SizedBox(height: DeviceInsetsCache.viewPaddingTop + 6),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
